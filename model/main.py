d0a39824b0dd470b97500a64233e08bd4bf4fdbc
Cloudfurys <90815489+Comethruuu@users.noreply.github.com>
2023-07-14 06:34:19 +0700

    main.py

--- model/main.py
+++ model/main.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 import pickle5 as pickle
-
+ 
 
 def create_model(data): 
   X = data.drop(['diagnosis'], axis=1)
